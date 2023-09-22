f379eb48b22712b1a78325289fc425acdcbd4ddb
kymoodl022 <kymoodl022@student.wethinkcode.co.za>
2023-09-22 13:31:17 +0200

    nav bar layout made

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,51 @@
 <script>
     /** @type {import('./$types').LayoutData} */
-    export let data;
 </script>
+<ul class=navbar>
+ <li>
+    <a href="/profile/" >
+        profile
+    </a>
+</li>
+<li>
+    <a href="/search/">
+        search
+    </a>
+</li>
+<li>
+    <a href="/watchlist/">
+        watchlist
+    </a>
+</li>
+<li>
+    <a href="/featured/">
+        featured
+    </a>
+</li>
+
+</ul>
+<style>
+    li{
+        float: left;
+    }
+.navbar{
+    display: flex;
+    justify-content: space-evenly;
+    
+
+}
+li a{
+    background-color: black;
+    display: block;
+    color:aliceblue;
+    text-decoration: none;
+}
+li a:hover{
+    background-color: aqua;
+    color:black
+}
+.active{
+    background-color: blueviolet;
+}
+</style>    
 <slot/>
-<nav>
-    <div>
-        <nav>
-            <a href="./profile/" >
-                <p>profile</p>
-            </a>
-            <a href="./search/">
-                <p>search</p>
-            </a>
-            <a href="./watchlist/">
-                <p>watchlist</p>
-            </a>
-            <a href="./featured/">
-                <p>featured</p>
-            </a>
-            </nav>
-            
-    </div>
-</nav>
