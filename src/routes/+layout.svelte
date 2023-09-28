a341adef8ac40078e59f1265d9e91abb207aea1a
kymoodl022 <kymoodl022@student.wethinkcode.co.za>
2023-09-28 14:57:37 +0200

    ground up navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,31 +20,39 @@
     
   
 </script>
-<div class="flex navbar bg-base-100 ">
-    <div class="navbar-start">
-      <div class="dropdown">
-        <label tabindex="0" class="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-        </label>
-        <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <li><a href="/featured/">Homepage</a></li>
-          <li><a href="/profile/">Profile</a></li>
-          <li><a href="/watchlist/">watchlist</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="navbar-center">
-      <a class="btn btn-ghost normal-case text-xl" href="/featured/">TVS For Devs</a>
-    </div>
-    <div class="navbar-end">
-        <div class="flex space-x-0">
-          <button on:click={search} class="btn btn-ghost btn-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-          </button>
-            <input on:keyup={searchEnter} input="text" placeholder="Search" bind:value={searchedShow}   class="input max-w-xs" />
-            
+<nav  class="p-5 bg-white shadow md:flex md:items-center z-100 md:justify-between " >
+  <div>
+    <span>
+      <a  class="mx-4 text-lg text-teal-400"href="#">logo</a>
+    </span>
+  </div>
+  <ul class="md:flex md:items-center">
+    <li class="mx-4">
+      <a class=" text-lg text-teal-400" href="#">logo</a>
+    </li>
+    <li class="mx-4">
+      <a class=" text-lg text-teal-400" href="#">logo</a>
+    </li>   
+     <li class="mx-4 ">
+      <a class=" text-lg text-teal-400" href="#">logo</a>
+    </li>    <li class="mx-4 ">
+      <a class=" text-lg text-teal-400" href="#">logo</a>
+    </li> 
+    <li class="mx-4">
+      <a class=" text-lg text-teal-400" href="#">logo</a>
+    </li>
+    <li class="mx-4">
+      <a class=" text-lg text-teal-400" href="#">logo</a>
+    </li>   
+     <li class="mx-4 ">
+      <a class=" text-lg text-teal-400" href="#">logo</a>
+    </li>    <li class="mx-4 ">
+      <a class=" text-lg text-teal-400" href="#">logo</a>
+    </li> 
+  </ul>
+</nav>
 
-        </div>
-    </div>
-  </div>  
+
+
+
 <slot/>
